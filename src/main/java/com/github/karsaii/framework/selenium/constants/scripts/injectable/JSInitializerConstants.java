f7c8f952c162a2e1e21eddb4b30919398fe3586b
Istvan Karsai <istvan.karsai.public@gmail.com>
2020-09-07 03:01:58 +0200

    Updated the thorium utilities, so also updating the reference to the latest version. (#28)

--- src/main/java/com/github/karsaii/framework/selenium/constants/scripts/injectable/JSInitializerConstants.java
+++ src/main/java/com/github/karsaii/framework/selenium/constants/scripts/injectable/JSInitializerConstants.java
@@ -1,7 +1,7 @@
 package com.github.karsaii.framework.selenium.constants.scripts.injectable;
 
 public abstract class JSInitializerConstants {
-    private static final String BASE = "https://cdn.jsdelivr.net/gh/karsaii/thorium-selenium@25010741512f1599cc321b0287627ca8904a2193/js-utils/";
+    private static final String BASE = "https://cdn.jsdelivr.net/gh/karsaii/thorium-selenium@e767aa3b896741077621fd97e4f6d0c428d9f5ff/js-utils/";
     public static final String STRINGULARITY_TYPE_UTILS = BASE + "stringularity-type-utils-nodocs-rollup.js";
     public static final String THORIUM_UTILITIES = BASE + "thoriumutilities.js";
 
